490b0870fef224faf2c0fecd1fc72a664dc3fcd8
Naksyug <jpmonillas@gmail.com>
2025-06-13 10:22:33 +0800

    Delete RSM-6-13-2025/RSM-main directory

--- RSM-6-13-2025/RSM-main/rms/add_ingredient.php
+++
@@ -1,7 +0,0 @@
-<?php
-$pdo = new PDO("mysql:host=localhost;dbname=bjm_menu", "root", "");
-$sql = "INSERT INTO ingredients (name, quantity) VALUES (?, ?)";
-$stmt = $pdo->prepare($sql);
-$stmt->execute([$_POST['name'], $_POST['quantity']]);
-header("Location: ingredients.php");
-exit;
