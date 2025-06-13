78103f4a8e563f475088877a07a617c6f8940f89
Naksyug <jpmonillas@gmail.com>
2025-06-13 10:22:43 +0800

    Delete rms directory

--- rms/delete_ingredient.php
+++
@@ -1,7 +0,0 @@
-<?php
-$pdo = new PDO("mysql:host=localhost;dbname=bjm_menu", "root", "");
-$sql = "DELETE FROM ingredients WHERE id = ?";
-$stmt = $pdo->prepare($sql);
-$stmt->execute([$_GET['id']]);
-header("Location: ingredients.php");
-exit;
